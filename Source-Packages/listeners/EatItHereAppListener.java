69278f6b8227626217c5d1c655bcc09fd059b65a
J4m41 <gianma_95@yahoo.it>
2016-05-24 18:30:50 +0200

    Added correct Listener

--- Source-Packages/listeners/EatItHereAppListener.java
+++ Source-Packages/listeners/EatItHereAppListener.java
@@ -5,6 +5,9 @@
  */
 package listeners;
 
+import db_classes.DBManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -19,11 +22,20 @@ public class EatItHereAppListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String dburl = sce.getServletContext().getInitParameter("dburl");
+        String dbuser = sce.getServletContext().getInitParameter("dbuser");
+        String dbpassword = sce.getServletContext().getInitParameter("dbpassword");
+        try{
+            DBManager manager = new DBManager(dburl, dbuser, dbpassword);
+            sce.getServletContext().setAttribute("dbmanager", manager);
+        } catch (SQLException ex){
+            Logger.getLogger(getClass().getName()).severe(ex.toString());
+            throw new RuntimeException(ex);
+        }
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        DBManager.shutdown();
     }
 }
